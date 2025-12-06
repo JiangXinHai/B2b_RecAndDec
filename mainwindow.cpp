60c767e443eed2f64d1445ecdf4d420bd41390f4
xinhai <3501408827@qq.com>
2025-12-06 22:57:55 +0800

    update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,149 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
+﻿#include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    , ui(new Ui::MainWindow)  // 初始化UI对象
+    , m_communicator(new Communicator(this))
 {
+    // 加载Qt Designer设计的UI
     ui->setupUi(this);
+
+    // ========== 初始化控件 ==========
+    // 填充串口列表（Windows）
+    foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts()) {
+        ui->cbx_SerialPort->addItem(info.portName());
+    }
+
+    // 初始化按钮状态
+    ui->btn_Start->setEnabled(true);
+    ui->btn_Stop->setEnabled(false);
+
+    // ========== 绑定信号槽 ==========
+    // 按钮点击信号
+    connect(ui->btn_Start, &QPushButton::clicked, this, &MainWindow::onStartBtnClicked);
+    connect(ui->btn_Stop, &QPushButton::clicked, this, &MainWindow::onStopBtnClicked);
+    connect(ui->btn_BrowseFile, &QPushButton::clicked, this, &MainWindow::onBrowseFileBtnClicked);
+
+    // 通讯器信号
+    connect(m_communicator, &Communicator::dataReady, this, &MainWindow::onDataReady);
+    connect(m_communicator, &Communicator::errorOccurred, this, &MainWindow::onErrorOccurred);
+    connect(m_communicator, &Communicator::stateChanged, this, &MainWindow::onStateChanged);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+    delete ui;  // 释放UI对象
 }
 
+// ========== 按钮点击槽函数 ==========
+void MainWindow::onStartBtnClicked()
+{
+    // 构建配置
+    Communicator::Config config = buildConfig();
+
+    // 确定通讯类型
+    Communicator::CommunicationType type;
+    if (ui->radioButton_file->isChecked()) {
+        type = Communicator::CommunicationType::File;
+    } else if (ui->radioButton_Tcp->isChecked()) {
+        type = Communicator::CommunicationType::TcpClient;
+    } else {
+        type = Communicator::CommunicationType::SerialPort;
+    }
+
+    // 启动通讯
+    bool success = m_communicator->startCommunication(type, config);
+    if (success) {
+        ui->te_Log->append(QString("[%1] 启动%2模式成功")
+                          .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"))
+                          .arg(type == Communicator::CommunicationType::File ? "文件" :
+                               (type == Communicator::CommunicationType::TcpClient ? "TCP客户端" : "串口")));
+    } else {
+        ui->te_Log->append(QString("[%1] 启动%2模式失败")
+                          .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"))
+                          .arg(type == Communicator::CommunicationType::File ? "文件" :
+                               (type == Communicator::CommunicationType::TcpClient ? "TCP客户端" : "串口")));
+    }
+}
+
+void MainWindow::onStopBtnClicked()
+{
+    m_communicator->stopCommunication();
+    ui->te_Log->append(QString("[%1] 手动停止通讯")
+                      .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss")));
+}
+
+void MainWindow::onBrowseFileBtnClicked()
+{
+    QString filePath = QFileDialog::getOpenFileName(this, "选择B2b卫星数据文件", "", "所有文件 (*.*);;二进制文件 (*.bin)");
+    if (!filePath.isEmpty()) {
+        ui->editFilePath->setText(filePath);
+    }
+}
+
+// ========== 通讯器信号槽函数 ==========
+void MainWindow::onDataReady(const QByteArray &rawData)
+{
+    // 显示十六进制数据
+    QString hexData = rawData.toHex(' ').toUpper();
+    ui->te_HexData->append(QString("[%1] 接收数据：%2 (长度：%3字节)")
+                          .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"))
+                          .arg(hexData)
+                          .arg(rawData.size()));
+
+    // 自动滚动到底部
+    QScrollBar *scroll = ui->te_HexData->verticalScrollBar();
+    scroll->setValue(scroll->maximum());
+}
+
+void MainWindow::onErrorOccurred(const QString &errorMsg)
+{
+    ui->te_Log->append(QString("[%1] 【错误】%2")
+                      .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"))
+                      .arg(errorMsg));
+
+    // 自动滚动到底部
+    QScrollBar *scroll = ui->te_Log->verticalScrollBar();
+    scroll->setValue(scroll->maximum());
+}
+
+void MainWindow::onStateChanged(bool isRunning)
+{
+    // 更新按钮状态
+    ui->btn_Start->setEnabled(!isRunning);
+    ui->btn_Stop->setEnabled(isRunning);
+
+    // 日志提示
+    if (isRunning) {
+        ui->te_Log->append(QString("[%1] 通讯已启动")
+                          .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss")));
+    } else {
+        ui->te_Log->append(QString("[%1] 通讯已停止")
+                          .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss")));
+    }
+}
+
+// ========== 构建配置参数 ==========
+Communicator::Config MainWindow::buildConfig()
+{
+    Communicator::Config config;
+
+    // 文件模式配置
+    config.filePath = ui->editFilePath->text();
+    config.readBlockSize = ui->spinBlockSize->value();
+    config.readInterval = ui->spinReadInterval->value();
+
+    // TCP客户端配置
+    config.tcpIp = ui->editIcpIp->text();
+    config.tcpPort = ui->spinIcpPort->value();
+
+    // 串口配置
+    config.serialPortName = ui->cbx_SerialPort->currentText();
+    config.baudRate = ui->cbx_BaudRate->currentText().toInt();
+    config.dataBits = QSerialPort::Data8;
+    config.parity = QSerialPort::NoParity;
+    config.stopBits = QSerialPort::OneStop;
+    config.flowControl = QSerialPort::NoFlowControl;
+
+    return config;
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,11 +1,16 @@
-#ifndef MAINWINDOW_H
+﻿#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPortInfo>
+#include <QDateTime>
+#include <QScrollBar>
+#include <QFileDialog>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+// 引入Qt Designer生成的UI头文件
+#include "ui_mainwindow.h"
+
+#include "Communicator.h"
 
 class MainWindow : public QMainWindow
 {
@@ -13,9 +18,28 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    ~MainWindow() override;
+
+private slots:
+    // 按钮点击槽函数
+    void onStartBtnClicked();
+    void onStopBtnClicked();
+    void onBrowseFileBtnClicked();
+
+    // 通讯器信号槽函数
+    void onDataReady(const QByteArray &rawData);
+    void onErrorOccurred(const QString &errorMsg);
+    void onStateChanged(bool isRunning);
 
 private:
+    // 构建配置参数（根据选中的通讯类型）
+    Communicator::Config buildConfig();
+
+    // Qt Designer生成的UI对象（核心）
     Ui::MainWindow *ui;
+
+    // 通讯器核心对象
+    Communicator *m_communicator;
 };
+
 #endif // MAINWINDOW_H
